4cd4cc7b20efe68b8c790afafbeff57bc4a39360
wdafifah <wahyuafifah1@gmail.com>
2017-11-07 09:02:06 +0700

    login

--- app/Admin.php
+++ app/Admin.php
@@ -5,11 +5,11 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admins extends Authenticatable
+class Admin extends Authenticatable
 {
     use Notifiable;
 	
-	protected $guard = ‘admin’;
+	protected $guard = 'admin';
 
     /**
      * The attributes that are mass assignable.
